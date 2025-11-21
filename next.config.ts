25f83ec1260b29c0f07ef39b461ad2d15716e2a5
Rene <neerwright@gmail.com>
2025-11-21 14:18:59 +0100

    changes supabase bucket

--- next.config.ts
+++ next.config.ts
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "evxbpuitidvqssfpdwyk.supabase.co",
+        hostname: "euhaycuyelsdkgdjcags.supabase.co",
       },
       {
         protocol: "https",
